8a2a044f252c36ca66f6e137bcec9604e2b8d519
Jeremias Mastroianni <jeremiasmastroianni00@gmail.com>
2024-05-17 17:44:09 -0300

    Actualizando tareas

--- Tareas_Practicas.ts
+++ Tareas_Practicas.ts
@@ -4,14 +4,18 @@ let Altura: number= 10;
 let area: number= Base * Altura;
 console.log(area);
 console.log("El valor de mi rectangulo es " + area);*/ 
+
 /*Esto es una secuencia
 console.log("1-pescado");
 console.log("2-pescado");*/ 
+
 /*Comando: ts-node areaRectangulo.ts
 let numero: number=10;
 console.log(numero);*/
+
 /*Variable delcarada
 let mensaje: string;
 mensaje= "Hola Mundo";
 console.log(mensaje);*/ 
+
 /*Tareas nuevas*/
