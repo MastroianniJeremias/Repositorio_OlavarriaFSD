f01f7005e350be25c2b81f3bdf19036fa6ca40e2
Jeremias Mastroianni <jeremiasmastroianni00@gmail.com>
2024-07-27 16:06:10 -0300

    dos clases de sobremi

--- TP_FINAL/scripts.js
+++ TP_FINAL/scripts.js
@@ -7,7 +7,6 @@ const showCart = () => {
     alertCompras.classList.toggle("flex");
     alertCompras.innerHTML = 
     `<span><h1><i class="fi fi-rr-shop"></i>Tu carrito</h1></span>
-    // <video src="video/shopping-harley-quinn.mp4"> no funciona el video
     <div id="bottom-button"><h1>Total: ${total}</h1>
     <button class="button-hover">Finalizar compra</button></div>`
 } 
