ae081f609a5b47b617a98df6497ddbafeacf6d38
Jeremias Mastroianni <jeremiasmastroianni00@gmail.com>
2024-07-30 02:17:56 -0300

    aside

--- TP_FINAL/scripts.js
+++ TP_FINAL/scripts.js
@@ -3,13 +3,12 @@ const carrito = document.getElementById("superior-extrema-izquierda");
 let total = 0;
 
 const showCart = () => {
-    alertCompras.classList.toggle("hidden");
-    alertCompras.classList.toggle("flex");
-    alertCompras.innerHTML = 
-    `<span><h1><i class="fi fi-rr-shop"></i>Tu carrito</h1></span>
+  alertCompras.classList.toggle("hidden");
+  alertCompras.classList.toggle("flex");
+  alertCompras.innerHTML = `<span><h1><i class="fi fi-rr-shop"></i>Tu carrito</h1></span>
     <img src="Gif/Y3il.gif" alt="gif" title="Because when i shop, the world gets better">
     <div id="bottom-button"><h1>Total: ${total}</h1>
-    <button class="button-hover">Finalizar compra</button></div>`
-} 
+    <button class="button-hover">Finalizar compra</button></div>`;
+};
 
 carrito.addEventListener("click", showCart);
