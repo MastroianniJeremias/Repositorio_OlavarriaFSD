e7c1a34e48e6d99c1f6f07ab9f6fc3fb8fe66269
Jeremias Mastroianni <jeremiasmastroianni00@gmail.com>
2024-08-05 01:35:22 -0300

    final

--- TP_FINAL/scripts_compra.js
+++ TP_FINAL/scripts_compra.js
@@ -1,6 +1,5 @@
-"use strict";
-
 // Definimos los productos en arrays separados
+
 // La lista de productos, precios y stock de la caja de productos
 let nombresProductos = [
   "Morcilla Criolla",
@@ -64,81 +63,3 @@ let imagenesProductosTecnologia = [
   "Img/productos9.jpg",
   "Img/productos10.jpg",
 ];
-
-document.addEventListener("DOMContentLoaded", () => {
-  let totalTotal = parseFloat(sessionStorage.getItem("total-total")) || 0.0;
-  document.getElementById("total-total").textContent = totalTotal.toFixed(2);
-  console.log(`Monto total cargado desde sessionStorage: ${totalTotal}`);
-
-  let productContainers = document.querySelectorAll(".contenedor-principal");
-  productContainers.forEach((container, index) => {
-    if (
-      nombresProductos[index] &&
-      preciosProductos[index] &&
-      stockProductos[index] !== undefined
-    ) {
-      let imgElement = container.querySelector(".imagen-producto");
-      let productStockElement = container.querySelector(".product-stock");
-      let productNameElement = container.querySelector(".titulo-producto");
-      let productPriceElement = container.querySelector(".precio-producto");
-
-      if (productNameElement && productPriceElement) {
-        productStockElement.textContent = `Stock: ${stockProductos[index]}`;
-        productNameElement.textContent = `${nombresProductos[index]}`;
-        productPriceElement.textContent = `Precio: $${preciosProductos[index]}`;
-        imgElement.setAttribute("src", imagenesProductos[index]);
-        imgElement.setAttribute("alt", nombresProductos[index]);
-      }
-    }
-  });
-
-  let productContainersTecnologia = document.querySelectorAll(
-    ".contenedor-principal-tecnologia"
-  );
-  productContainersTecnologia.forEach((container, index) => {
-    if (
-      nombresProductosTecnologia[index] &&
-      preciosProductosTecnologia[index] &&
-      stockProductosTecnologia[index] !== undefined
-    ) {
-      let imgElement = container.querySelector(".imagen-producto");
-      let productStockElement = container.querySelector(".product-stock");
-      let productNameElement = container.querySelector(".titulo-producto");
-      let productPriceElement = container.querySelector(".precio-producto");
-
-      if (productNameElement && productPriceElement) {
-        productStockElement.textContent = `Stock: ${stockProductosTecnologia[index]}`;
-        productNameElement.textContent = `${nombresProductosTecnologia[index]}`;
-        productPriceElement.textContent = `Precio: $${preciosProductosTecnologia[index]}`;
-        imgElement.setAttribute("src", imagenesProductosTecnologia[index]);
-        imgElement.setAttribute("alt", nombresProductosTecnologia[index]);
-      }
-
-      let cantidadInput = container.querySelector(".producto-cantidad");
-      let addBtn = container.querySelector(".btn-p");
-
-      cantidadInput.addEventListener("input", () => {
-        if (cantidadInput.value < 0 || cantidadInput.value == "") {
-          cantidadInput.value = 0;
-        }
-      });
-
-      addBtn.addEventListener("click", () => {
-        let quantity = parseInt(cantidadInput.value);
-        if (quantity > stockProductosTecnologia[index]) {
-          alert("No hay suficiente stock para realizar la compra");
-        } else {
-          stockProductosTecnologia[index] -= quantity;
-          totalTotal += preciosProductosTecnologia[index] * quantity;
-          document.getElementById("total-total").textContent =
-            totalTotal.toFixed(2);
-          sessionStorage.setItem("total-total", totalTotal.toFixed(2));
-          console.log(
-            `Cantidad disponible. Stock restante: ${stockProductosTecnologia[index]}`
-          );
-          productStockElement.textContent = `Stock: ${stockProductosTecnologia[index]}`;
-        }
-      });
-    }
-  });
-});
